f38019b5f03194b8643063e90742971f7c1d9d14
Yomi Rich <ymueni@gmail.com>
2019-07-21 20:50:52 +0300

    Add tests for user and teardown

--- test.py
+++ test.py
@@ -1,4 +1,33 @@
 import unittest
-from user.py import User
+from user import User
 
 class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+     Args:
+         unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+
+        self.new_user =  User("Yomi","Instagram","Password")
+
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Yomi")
+        self.assertEqual(self.new_user.account,"Instagram")
+        self.assertEqual(self.new_user.password,"Password")
--- user.py
+++ user.py
@@ -1,2 +1,6 @@
 class User:
-    pass
+    def __init__(self, username,account,password):
+        
+        self.username = username
+        self.account = account
+        self.    
