ce7eed451c0b0aab62fb9b34269965ed41209965
Yomi Rich <ymueni@gmail.com>
2019-07-22 06:51:17 +0300

    Add save user method

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-
         self.new_user =  User("Yomi","Instagram","Password")
 
     def tearDown(self):
@@ -27,7 +26,13 @@ class TestUser(unittest.TestCase):
         '''
         test_init test case to test if the object is initialized properly
         '''
-
         self.assertEqual(self.new_user.username,"Yomi")
         self.assertEqual(self.new_user.account,"Instagram")
         self.assertEqual(self.new_user.password,"Password")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user()  # saving the new user
+        self.assertEqual(len(User.user_list), 1)
