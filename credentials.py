b093e79acc14ca933961547d4072f099dc1bc45b
Yomi Rich <ymueni@gmail.com>
2019-07-22 07:52:08 +0300

    Initialized the credentials list

--- credentials.py
+++ credentials.py
@@ -6,3 +6,9 @@ class credentials:
     class to save the user class information
     '''
     credentials_list = []
+
+    def __init__(self, name, username, password):
+        self.platform_name = name
+        self.user_name = username
+        self.password = password
+        
