5bb7c7dbe7b6f8178267a75aab7637645a2770f5
Yomi Rich <ymueni@gmail.com>
2019-07-22 07:01:48 +0300

    Add search user by name method

--- test.py
+++ test.py
@@ -55,3 +55,13 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.new_user.delete_user()  # delete function
         self.assertEqual(len(User.user_list), 1)
+
+    def test_find_user_by_username(self):
+        '''
+        test to check if we can find a user by username and display information
+        '''
+        self.new_user.save_user()
+        test_user = User("Instagram", "Queen", "91011")  # new user
+        test_user.save_user()
+        found_user = User.find_by_username("Queen")
+        self.assertEqual(found_user.password, test_user.password)
