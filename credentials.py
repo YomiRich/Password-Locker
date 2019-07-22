9c006d7dcfd6e9346efb72b210d537f808a6e453
Yomi Rich <ymueni@gmail.com>
2019-07-22 07:53:51 +0300

    Add delete credentials method

--- credentials.py
+++ credentials.py
@@ -11,9 +11,15 @@ class credentials:
         self.platform_name = name
         self.user_name = username
         self.password = password
-        
+
     def save_credentials(self):
         '''
         method to save credential to credential list
         '''
         credentials.credentials_list.append(self)
+        
+    def delete_credentials(self):
+        '''
+        method to delete credentials from credentials list
+        '''
+        credentials.credentials_list.remove(self)
