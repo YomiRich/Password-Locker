ef7aaa9a33bd4e77085cf0e02690b97182dce79b
Yomi Rich <ymueni@gmail.com>
2019-07-22 07:38:39 +0300

    Add test to check if multiple credentials can be added

--- test.py
+++ test.py
@@ -103,10 +103,20 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_credentials.platform_name, "Pinterest")
         self.assertEqual(self.new_credentials.user_name, "Iamrich")
         self.assertEqual(self.new_credentials.password, "1234")
-        
+
     def test_save_credentials(self):
         '''
         test case to test if the object is saved into the credentials_list
         '''
         self.new_credentials.save_credentials()
         self.assertEqual(len(credentials.credentials_list), 1)
+        
+    def test_save_multiple_credentials(self):
+        '''
+        test case to test if we can save multiple credentials into the credentials_list
+        '''
+        self.new_credentials.save_credentials()
+        test_credentials = credentials("test", "testname", "12345678")
+        test_credentials.save_credentials()
+
+        self.assertEqual(len(credentials.credentials_list), 2)
