395917e7f9d02e9be2267e672760ef9af75b052d
Yomi Rich <ymueni@gmail.com>
2019-07-22 07:14:02 +0300

    Add class method to search and return user

--- user.py
+++ user.py
@@ -23,4 +23,13 @@ class User:
         '''
         delete_user_method deletes a saved user from the user_list
         '''
-        User.user_list.remove(self)       
+        User.user_list.remove(self)
+        
+    @classmethod
+    def find_by_username(cls, username):
+        '''
+        method that takes in a username and returns a user that matches that username
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return user
